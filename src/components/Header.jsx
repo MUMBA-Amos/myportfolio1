92ca4b50703dedf0414f20f36c7d8cc9a3090ede
Your Name <you@example.com>
2024-08-20 17:50:21 +0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { Modal, Button } from "react-bootstrap";
+import React from "react";
 import CountUp from "react-countup";
 import { motion } from "framer-motion";
 import {
@@ -14,128 +13,106 @@ import "./Header.css";
 import mumbaImage from "./mumba.png";
 
 const Header = ({ contactRef }) => {
-  const [showModal, setShowModal] = useState(false);
-
-  const handleShow = () => setShowModal(true);
-  const handleClose = () => setShowModal(false);
+  const scrollToContact = () => {
+    contactRef.current.scrollIntoView({ behavior: "smooth" });
+  };
 
   return (
-    <>
-      <header className="header d-flex align-items-center">
-        <div className="container">
-          <Navbar />
-          <div className="hero-content row align-items-center text-white mt-5">
-            <div className="hero-image col-lg-4 col-md-5 col-sm-12 text-center mt-4 mt-md-0 order-1 order-md-2">
-              <div style={styles.photoContainer}>
-                <div style={styles.photo}>
-                  <img
-                    src={mumbaImage}
-                    alt="Mumba Amos Ntambo"
-                    style={styles.profileImage}
-                  />
-                  <motion.div
-                    style={styles.spinningBorder}
-                    animate={{ rotate: -360 }}
-                    transition={{
-                      repeat: Infinity,
-                      duration: 120,
-                      ease: "linear",
-                    }}
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="hero-text col-lg-8 col-md-7 col-sm-12 order-2 order-md-1">
-              <h1>
-                Hello I'm{" "}
-                <span className="text-success">Mumba Amos Ntambo</span>
-              </h1>
-              <p className="lead">Software Developer</p>
-              <p>
-                I excel at crafting elegant digital experiences and am
-                proficient in various programming languages and technologies.
-              </p>
-              <div className="d-flex align-items-center mt-3 justify-content-center justify-content-md-start">
-                <a
-                  href="/myportfolio1/cv.pdf"
-                  download="Mumba_Amos_Ntambo_CV.pdf"
-                  className="btn btn-success"
-                >
-                  View CV
-                </a>
-                <div className="social-icons ml-4">
-                  <a href="#!" onClick={handleShow} className="icon">
-                    <FaPhone />
-                  </a>
-                  <a
-                    href="https://www.linkedin.com/in/mumba-amos-ntambo-54a665214/"
-                    className="icon"
-                  >
-                    <FaLinkedin />
-                  </a>
-                  <a href="https://facebook.com" className="icon">
-                    <FaFacebook />
-                  </a>
-                  <a href="https://wa.me/yourwhatsapplink" className="icon">
-                    <FaWhatsapp />
-                  </a>
-                  <a href="https://github.com" className="icon">
-                    <FaGithub />
-                  </a>
-                </div>
+    <header className="header d-flex align-items-center">
+      <div className="container">
+        <Navbar />
+        <div className="hero-content row align-items-center text-white mt-5">
+          <div className="hero-image col-lg-4 col-md-5 col-sm-12 text-center mt-4 mt-md-0 order-1 order-md-2">
+            <div style={styles.photoContainer}>
+              <div style={styles.photo}>
+                <img
+                  src={mumbaImage}
+                  alt="Mumba Amos Ntambo"
+                  style={styles.profileImage}
+                />
+                <motion.div
+                  style={styles.spinningBorder}
+                  animate={{ rotate: -360 }}
+                  transition={{
+                    repeat: Infinity,
+                    duration: 120,
+                    ease: "linear",
+                  }}
+                />
               </div>
             </div>
           </div>
-          <div className="hero-stats d-flex justify-content-around flex-wrap mt-5">
-            <div className="p-2">
-              <h2 className="text-success">
-                <CountUp start={0} end={2.5} duration={2} />
-              </h2>
-              <p>Years of experience</p>
-            </div>
-            <div className="p-2">
-              <h2 className="text-success">
-                <CountUp start={0} end={8} duration={2} />
-              </h2>
-              <p>Projects completed</p>
-            </div>
-            <div className="p-2">
-              <h2 className="text-success">
-                <CountUp start={0} end={5} duration={2} />
-              </h2>
-              <p>Technologies mastered</p>
-            </div>
-            <div className="p-2">
-              <h2 className="text-success">
-                <CountUp start={0} end={20} duration={2} />
-              </h2>
-              <p>Code commits</p>
+          <div className="hero-text col-lg-8 col-md-7 col-sm-12 order-2 order-md-1">
+            <h1>
+              Hello I'm <span className="text-success">Mumba Amos Ntambo</span>
+            </h1>
+            <p className="lead">Software Developer</p>
+            <p>
+              I excel at crafting elegant digital experiences and am proficient
+              in various programming languages and technologies.
+            </p>
+            <div className="d-flex align-items-center mt-3 justify-content-center justify-content-md-start">
+              <a
+                href="/myportfolio1/cv.pdf"
+                download="Mumba_Amos_Ntambo_CV.pdf"
+                className="btn btn-success"
+              >
+                View CV
+              </a>
+              <div className="social-icons ml-4">
+                <a href="+60176307134" className="icon">
+                  <FaPhone />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/mumba-amos-ntambo-54a665214/"
+                  className="icon"
+                >
+                  <FaLinkedin />
+                </a>
+                <a href="https://facebook.com" className="icon">
+                  <FaFacebook />
+                </a>
+                <a
+                  href="https://wa.me/+60176307134?text=Hello%20Mumba!"
+                  className="icon"
+                >
+                  <FaWhatsapp />
+                </a>
+                <a href="https://github.com" className="icon">
+                  <FaGithub />
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </header>
-
-      {/* Modal for displaying the phone number */}
-      <Modal show={showModal} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Contact Me</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>You can call me at:</p>
-          <h4>+1234567890</h4> {/* Replace with your actual phone number */}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="success" href="tel:+1234567890">
-            {" "}
-            {/* Replace with your actual phone number */}
-            Call Now
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+        <div className="hero-stats d-flex justify-content-around flex-wrap mt-5">
+          <div className="p-2">
+            <h2 className="text-success">
+              <CountUp start={0} end={2.5} duration={2} />
+            </h2>
+            <p>Years of experience</p>
+          </div>
+          <div className="p-2">
+            <h2 className="text-success">
+              <CountUp start={0} end={8} duration={2} />
+            </h2>
+            <p>Projects completed</p>
+          </div>
+          <div className="p-2">
+            <h2 className="text-success">
+              <CountUp start={0} end={5} duration={2} />
+            </h2>
+            <p>Familiar Technologies</p>
+          </div>
+          <div className="p-2">
+            <h2 className="text-success">
+              <CountUp start={0} end={20} duration={2} />
+            </h2>
+            <p>Code commits</p>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
 
